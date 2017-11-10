3763cb823d430e538a5468df951deae363c5cf38
gabrielgyns <gabrielgyns@gmail.com>
2017-11-10 20:33:04 -0200

    iniciamos o projeto

---
+++ class/Sql.php
@@ -0,0 +1,37 @@
+<?php 
+
+class Sql extends PDO{
+	private $con;
+
+	# Quando fizer o "new", quando instanciar essa classe, será feito automaticamente o código abaixo, o método mágico construtor.
+	public function __construct(){
+		$this->con = new PDO("mysql: host=localhost; dbname=db_php7", "root", "");
+	}
+
+	private function setParams($statment, $parameters){
+		foreach ($parameters as $key => $value) {
+			
+			$this->setParam($key, $value);
+			
+		}
+	}
+
+	private function setParam($statment, $key, $value){
+		$statment->bindParam($key, $value);
+	}
+
+	public function query($rawQuery, $params = array()){
+		$stmt = $this->con->prepare($rawQuery);
+		$this->setParams($stmt, $params);
+		$stmt->execute();
+		return $stmt;
+	}
+
+	public function select($rawQuery, $params = array()):array{
+		$stmt = $this->query($rawQuery, $params);
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+}
+
+?>
