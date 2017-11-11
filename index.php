c9a1bf878db09f4cb736a5e68ceda969cdd36c44
gabrielgyns <gabrielgyns@gmail.com>
2017-11-10 22:25:40 -0200

    Atualizando projeto

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+/*$sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuario");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
+
+$user = new Usuario();
+$user->loadById(3);
+echo $user;
 
 ?>
