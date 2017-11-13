d4f964b491fafdc734eb7de66a800850ccff57da
gabrielgyns <gabrielgyns@gmail.com>
2017-11-13 01:30:59 -0200

    insert, update e delete

--- class/Usuario.php
+++ class/Usuario.php
@@ -34,6 +34,14 @@ class Usuario{
 		$this->dtcadastro = $dtcadastro;
 	}
 
+	//Metódo criado devido a utiliar várias vezes o "results" com os dados abaixo.
+	public function setData($data){
+		$this->setIdusuario($data['idusuario']);
+		$this->setDeslogin($data['deslogin']);
+		$this->setDessenha($data['dessenha']);
+		$this->setDtcadastro(new Datetime($data['dtcadastro']));
+	}
+
 	public function loadById($id){
 		$sql = new Sql();
 		$result = $sql->select("SELECT * FROM tb_usuario WHERE idusuario = :ID", array(
@@ -41,11 +49,7 @@ class Usuario{
 		));
 
 		if (isset($result[0])) {
-			$row = $result[0];
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new Datetime($row['dtcadastro']));
+			$this->setData($result[0]);
 		}
 	}
 
@@ -57,16 +61,65 @@ class Usuario{
 		));
 
 		if (isset($result[0])) {
-			$row = $result[0];
-			$this->setIdusuario($row['idusuario']);
-			$this->setDeslogin($row['deslogin']);
-			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new Datetime($row['dtcadastro']));
+			$this->setData($result[0]);
 		} else {
 			throw new Exception("Login e/ou senha inválidos.");
-			
 		}
 	}
+
+	/* 
+	## Não está funcionando chamar pela procedure como na video aula, então criei o método "isnert" que funciona.
+	## Seria interessante fazer por procedure, pois não iria precisar fazer tantas requisições no banco, evitaria lentidão.
+
+	public function cadastro(){
+		$sql = new Sql();
+		$result_test = $sql->select("CALL sp_usuarios_insert(:DESLOGIN, :DESSENHA)", array(
+			':DESLOGIN'=>$this->getDeslogin(),
+			':DESSENHA'=>$this->getDessenha()
+		));
+
+		$result = $sql->select("SELECT * FROM tb_usuario WHERE idusuario = LAST_INSERT_ID();");
+
+		if (isset($result[0])) {
+			$this->setData($result[0]);
+		}
+	}
+	## Meu método INSERT abaixo:           */
+	public function insert(){
+		$sql = new Sql();
+		$test = $sql->select("INSERT INTO tb_usuario (deslogin, dessenha) VALUES (:DESLOGIN, :DESSENHA);", array(
+			':DESLOGIN'=>$this->getDeslogin(),
+			':DESSENHA'=>$this->getDessenha()
+		));
+
+		$result = $sql->select("SELECT * FROM tb_usuario WHERE idusuario = LAST_INSERT_ID();");
+
+		if (isset($result[0])) {
+			$this->setData($result[0]);
+		}
+	}
+
+	public function update($login, $password){
+		$this->setDeslogin($login);
+		$this->setDessenha($password);
+		$sql = new Sql();
+		$sql->query("UPDATE tb_usuario SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+			':LOGIN'=>$this->getDeslogin(),
+			':PASSWORD'=>$this->getDessenha(),
+			':ID'=>$this->getIdusuario()
+		));
+	}	
+	public function delete(){
+		$sql = new Sql();
+		$sql->query("DELETE FROM tb_usuario WHERE idusuario = :ID", array(
+			':ID'=>$this->getIdusuario()
+		));
+
+		$this->setIdusuario(0);
+		$this->setDeslogin("");
+		$this->setDessenha("");
+		$this->setDtcadastro(new DateTime());
+	}	
 
 	public static function getList(){
 		$sql = new Sql();
--- index.php
+++ index.php
@@ -6,25 +6,45 @@ require_once("config.php");
 $usuarios = $sql->select("SELECT * FROM tb_usuario");
 
 echo json_encode($usuarios);*/
-
+##################################################################
 //Carrega um usuário
 //$user = new Usuario();
 //$user->loadById(3);
 //echo $user;
-
+##################################################################
 //Carrega uma lista de usuário
 //$lista = Usuario::getList();
 //echo json_encode($lista);
-
+##################################################################
 //Carrega uma lista de usuarios buscando pelo login
 //$busca = Usuario::search("y");
 //echo json_encode($busca);
-
+##################################################################
 //Faz login no sistema
-$login = new Usuario();
-$login->login("gabriel", "254654");
+//$login = new Usuario();
+//$login->login("gabriel", "254654");
 
-echo $login;
+//echo $login;
+##################################################################
+//Faz cadastro no sistema
+//$aluno = new Usuario();
 
+//$aluno->setDeslogin("aluno_2");
+//$aluno->setDessenha("outro");
 
+//$aluno->insert();
+
+//echo $aluno;
+##################################################################
+//Alterar um usuario
+/*$usuario = new Usuario();
+$usuario->loadById(14);
+$usuario->update("teste", "587987");
+echo $usuario;*/
+##################################################################
+//Deletando do banco
+$usuario = new Usuario();
+$usuario->loadById(16);
+$usuario->delete();
+echo $usuario;
 ?>
